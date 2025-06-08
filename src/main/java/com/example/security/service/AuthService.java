4d38bb917526b6cc2ef9497d0241b16ef03243dd
HanifFauzan-n <haniffauzann55@gmail.com>
2025-06-08 13:55:38 +0700

    Update Service

--- src/main/java/com/example/security/service/AuthService.java
+++ src/main/java/com/example/security/service/AuthService.java
@@ -1,15 +1,18 @@
 package com.example.security.service;
 
-import java.util.Optional;
+import java.util.Set;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.example.security.dto.LoginDto;
 import com.example.security.dto.RequestDto;
+import com.example.security.dto.ResponseDto;
 import com.example.security.entities.Role;
 import com.example.security.entities.Users;
 import com.example.security.repositories.RoleRepository;
 import com.example.security.repositories.UsersRepository;
+import com.example.security.utils.JwtUtils;
 
 import lombok.RequiredArgsConstructor;
 
@@ -20,22 +23,37 @@ public class AuthService {
     private final UsersRepository usersRepository;
     private final RoleRepository roleRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JwtUtils jwtUtils;
 
-    public String register(RequestDto dto) {
+    public ResponseDto register(RequestDto dto) {
         if (usersRepository.findByUsername(dto.getUsername()).isPresent())
             throw new RuntimeException("Username Already exists");
 
-        Optional<Role> optionalRole = roleRepository.findByName("ROLE_USERS");
-
-        Role role = optionalRole.orElseGet(() -> roleRepository.save(new Role(null, "ROLE_USERS")));
+        Role role = roleRepository.findByName("ROLE_USERS")
+                .orElseGet(() -> roleRepository.save(new Role(null, "ROLE_USERS")));
 
         Users user = new Users();
 
         user.setUsername(dto.getUsername());
+        user.setEmail(dto.getEmail());
         user.setPassword(passwordEncoder.encode(dto.getPassword()));
-        user.getRoles().add(role);
+        user.setRoles(Set.of(role));
 
         usersRepository.save(user);
-        return "User registered successfully";
+
+        String token = jwtUtils.generateToken(user.getUsername());
+        return new ResponseDto(token);
+    }
+
+    public ResponseDto login(LoginDto dto) {
+        Users user = usersRepository.findByUsername(dto.getUsername())
+                .orElseThrow(() -> new RuntimeException("Username tidak ditemukan"));
+
+        if (!passwordEncoder.matches(dto.getPassword(), user.getPassword())) {
+            throw new RuntimeException("Password salah");
+        }
+
+        String token = jwtUtils.generateToken(user.getUsername());
+        return new ResponseDto(token);
     }
 }
