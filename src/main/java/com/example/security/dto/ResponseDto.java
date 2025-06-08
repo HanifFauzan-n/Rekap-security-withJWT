9ef8f94b06f4821f60bd6da724043c65e32a7eec
HanifFauzan-n <haniffauzann55@gmail.com>
2025-06-08 13:55:57 +0700

    Update DTO

--- src/main/java/com/example/security/dto/ResponseDto.java
+++ src/main/java/com/example/security/dto/ResponseDto.java
@@ -1,8 +1,12 @@
 package com.example.security.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ResponseDto {
     private String token;
 }
