c5733196274fa1d2abfe48316d38816782564b31
HanifFauzan-n <haniffauzann55@gmail.com>
2025-06-09 21:40:48 +0700

    change example

--- src/main/java/com/example/security/controller/ExampleController.java
+++ src/main/java/com/example/security/controller/ExampleController.java
@@ -1,14 +1,24 @@
 package com.example.security.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class ExampleController {
     
     @GetMapping("/hello")
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
     public ResponseEntity<String> hello() {
         return ResponseEntity.ok("Halo! Kamu sudah login.");
     }
+
+    @GetMapping("/profile")
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
+    public ResponseEntity<String> userAcces() {
+        return ResponseEntity.ok("Hello, Ini Halaman Profile");
+    }
+    
 }
