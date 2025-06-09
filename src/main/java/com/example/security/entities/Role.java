88cb5051f27e54e5362a9350e735aa2026abad28
HanifFauzan-n <haniffauzann55@gmail.com>
2025-06-09 21:40:08 +0700

    update Role

--- src/main/java/com/example/security/entities/Role.java
+++ src/main/java/com/example/security/entities/Role.java
@@ -1,5 +1,6 @@
 package com.example.security.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -17,5 +18,6 @@ public class Role {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false, unique = true)
     private String name;
 }
