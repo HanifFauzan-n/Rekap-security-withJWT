d57da5928050fb2b3632f29ad3b38ea89c920a7a
HanifFauzan-n <haniffauzann55@gmail.com>
2025-06-11 23:39:19 +0700

    Update (Add) fiture in user

--- src/main/java/com/example/security/controller/UserController.java
+++ src/main/java/com/example/security/controller/UserController.java
@@ -1,18 +1,24 @@
 package com.example.security.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import lombok.RequiredArgsConstructor;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/user")
 public class UserController {
-    
+
     @GetMapping("/user")
     public ResponseEntity<String> userAcces() {
         return ResponseEntity.ok("Hello User");
     }
-    
+
+   
+   
+
+
 }
